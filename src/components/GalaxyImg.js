9630bc965cec8edd7dbcafb8d736640ac9914288
Sharo <sharodubey1312@gmail.com>
2023-10-15 03:23:44 +0530

    Home page and navbar created

---
+++ src/components/GalaxyImg.js
@@ -0,0 +1,22 @@
+import "./GalaxyImgStyles.css";
+import Galaxy from "../assets/bg.png"
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const GalaxyImg = () => {
+  return (
+    <div className="galaxy">
+        <div className="mask">
+            <img className="intro-bg" src = {Galaxy} alt="Galaxy" />
+        </div>
+        <div className="content">
+            <p>Hi, I am Freelancer.</p>
+            <h1>Cybersecurity Analyst and Python Developer.</h1>
+            <Link to = "/Projects" className="btn">Projects</Link>
+            <Link to = "/About" className="btn-light">About</Link>
+        </div>
+    </div>
+  )
+}
+
+export default GalaxyImg
---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import "./NavbarStyles.css";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if(window.scrollY >= 100) {
+            setColor(true);
+        } else {
+            setColor(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", changeColor);
+        return () => {
+            window.removeEventListener("scroll", changeColor);
+        };
+    }, []);
+
+    return (
+        <div className={color ? "header-bg" : "header"}>
+            <Link to="/">
+                <h1>Sharo</h1>
+            </Link>
+            <ul className={click ? "nav-menu" : "nav-menu active"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/Projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/About">About</Link>
+                </li>
+                <li>
+                    <Link to="/Contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (<FaBars size={20} style={{color: "#ffffff"}} />) : (<FaTimes size={20} style={{color: "#ffffff"}} />)}
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
